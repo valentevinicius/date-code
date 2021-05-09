a69c7613af2ef3a2d7520273d46676916d1b7624
Vinicius Valente <viniciusvramirez0108@gmail.com>
2021-05-09 00:53:36 -0300

    Improve folders

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function carregar(){
-    let msg = document.getElementById('msg');
-    let img = document.getElementById('imagem');
+    let msg = document.querySelector('#msg');
+    let img = document.querySelector('#imagem');
     let data = new Date();
     let hora = data.getHours();
 
@@ -8,15 +8,15 @@ function carregar(){
     msg.innerHTML = `Agora sao ${hora} horas.`
 
     if (hora >= 0 && hora <12){
-        img.src = 'fotomanha.png'
+        img.src = 'assets/images/fotomanha.jpg'
         //BOM DIA
         document.body.style.background = '#e2cd9f'
     } else if (hora >=12 && hora <18) {
-        img.src = 'fototarde.png'
+        img.src = 'assets/images/fototarde.png'
         //BOA TARDE
         document.body.style.background = '#b9846f'
     } else {
-        img.src = 'fotonoite.png'
+        img.src = 'assets/images/fotonoite.png'
         //BOA NOITE
         document.body.style.background = '#515154'
     }
