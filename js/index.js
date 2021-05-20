e092214497af012b3b494542c7d217ded740e353
Vinicius Valente <viniciusvramirez0108@gmail.com>
2021-05-20 17:43:10 -0300

    conversion date fix

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const getDollarValue = date => {
-    const urlBuilder = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%27${date}%27&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao`
+    const urlBuilder = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%27${date}%27&$top=1&$format=json&$select=cotacaoCompra,dataHoraCotacao`
     return fetch(urlBuilder).then(data => {
         return data.json()
     }).then(result => {
@@ -8,27 +8,27 @@ const getDollarValue = date => {
     }).catch(err =>{
         console.error('[ERRO]:', err)
     })
-}
+} //Funcao de acesso e manipulacao dos dados em json
 function carregar(){
     let msg = document.querySelector('#msg').children[0];
     let img = document.querySelector('#photo');
-    let data = new Date();
-    let hora = data.getHours();
+    let day = new Date();
+    let hour = day.getHours();
 
     //Alterar cor e imagem, de acordo com o horario
-    if (hora >= 0 && hora <12){
-        msg.innerHTML = `Bom dia, agora sao ${hora} horas.`
+    if (hour >= 0 && hour <12){
+        msg.innerHTML = `Bom dia, agora sao ${hour} horas.`
         img.src = 'assets/images/fotomanha.jpg'
         //BOM DIA
         document.body.style.background = '#FDD692'
-    } else if (hora >=12 && hora <18) {
-        msg.innerHTML = `Boa tarde, agora sao ${hora} horas.`
-        img.src = 'assets/images/fototarde.png'
+    } else if (hour >=12 && hour <18) {
+        msg.innerHTML = `Boa tarde, agora sao ${hour} horas.`
+        img.src = 'assets/images/fototarde.jpg'
         //BOA TARDE
         document.body.style.background = '#EC7357'
     } else {
-        msg.innerHTML = `Boa noite, agora sao ${hora} horas.`
-        img.src = 'assets/images/fotonoite.png'
+        msg.innerHTML = `Boa noite, agora sao ${hour} horas.`
+        img.src = 'assets/images/fotonoite.jpg'
         //BOA NOITE
         document.body.style.background = '#754F44'
     }
@@ -41,11 +41,11 @@ function converter() {
     let option = document.getElementsByName('radcurrency');
 
     if (option[0].checked) {
-        const x = getDollarValue('05-07-2021')
-        let dolarRes = Number(usdtxt.value) * 5
+        const parseInt(x) = getDollarValue('05-20-2021') //Acesso a funcao
+        let dolarRes = x
         option = 'Dolar'
         dolar.src = 'assets/images/icon-dolar.png'
-        res.innerHTML = `Detectamos ${option}, valor convertido: ${dolarRes} reais`
+        res.innerHTML = `Detectamos ${option}, valor convertido: ${dolarRes.toFixed(0)} reais`
     } else if (option[1].checked) {
         let bitcoinRes = Number(usdtxt.value) * 255419.13
         option = 'Bitcoin'
